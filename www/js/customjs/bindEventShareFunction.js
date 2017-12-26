d8bdc7cf9acd32cc561d41ea8d394b170b11142e
vcshing <vcshing2005@hotmail.com>
2017-12-26 22:47:04 +0800

    asd

--- www/js/customjs/bindEventShareFunction.js
+++ www/js/customjs/bindEventShareFunction.js
@@ -1,11 +1,34 @@
-$(".imgclap").bind("click",function(){
-  ion.sound.stop("audience_boo");
-ion.sound.play("clapping_and_cheers");
+var clappingMP3 = "clapping_and_cheers"
+
+$(".imgclap").bind("click", function() {
+    ion.sound.stop("audience_boo");
+    if (clappingType == "A") {
+        ion.sound.stop(clappingMP3);
+        ion.sound.play(clappingMP3);
+    }
+})
+
+$(".imgjeer").bind("click", function() {
+    ion.sound.stop(clappingMP3);
+    ion.sound.stop("audience_boo");
+    ion.sound.play("audience_boo");
 
 })
 
-$(".imgjeer").bind("click",function(){
-  ion.sound.stop("clapping_and_cheers");
-ion.sound.play("audience_boo");
+$(".titleButton").bind("click", function() {
+    ion.sound.stop(clappingMP3);
+    ion.sound.stop("audience_boo");
 
+})
+
+$(".clappingTypeA").bind("click", function() {
+    $(this).addClass("button-fill")
+    $(".clappingTypeB").removeClass("button-fill")
+    clappingMP3 = "clapping_and_cheers"
+})
+
+$(".clappingTypeB").bind("click", function() {
+    $(this).addClass("button-fill")
+    $(".clappingTypeA").removeClass("button-fill")
+    clappingMP3 = "clapping"
 })
--- www/js/customjs/langArr.js
+++ www/js/customjs/langArr.js
@@ -6,11 +6,11 @@ var menuSiteArr= []
 var loopLangArr = []
 
 appConfigArr={
-  "appName" :	"Best Choice",
-  "appDescription"	:	"Best Choice",
-  "androidAppLink"	:"https://play.google.com/store/apps/details?id=com.skyexplorer.goodprice",
-  "androidAdmobBannerID"	:"ca-app-pub-3715336230214756/1437729084",
-  "androidAdmobInterstitialID"	:"ca-app-pub-3715336230214756/8549932345"
+  "appName" :	"Cheer or Jeer",
+  "appDescription"	:	"Cheer or Jeer",
+  "androidAppLink"	:"https://play.google.com/store/apps/details?id=com.skyexplorer.cheerorjeer",
+  "androidAdmobBannerID"	:"ca-app-pub-3715336230214756/4252746061",
+  "androidAdmobInterstitialID"	:"ca-app-pub-3715336230214756/6930880290"
 }
 
 
@@ -18,40 +18,13 @@ appConfigArr={
 defaultLangArr.push("Other Apps");
 defaultLangArr.push("Share Apps");
 defaultLangArr.push("Default Language");
-defaultLangArr.push("計算");
 
-defaultLangArr.push("產品A");
-defaultLangArr.push("產品B");
-defaultLangArr.push("金額");
-defaultLangArr.push("金額");
-defaultLangArr.push("金額");
-defaultLangArr.push("數量");
-defaultLangArr.push("數量");
-defaultLangArr.push("數量");
-defaultLangArr.push("每件重量");
-defaultLangArr.push("每件重量");
-defaultLangArr.push("每件重量");
-defaultLangArr.push("建議");
-defaultLangArr.push("最便宜");
-
-defaultLangArr.push("產品A");
-defaultLangArr.push("每一元");
-defaultLangArr.push("每一單位");
-
-defaultLangArr.push("產品B");
-defaultLangArr.push("每一元");
-defaultLangArr.push("每一單位");
-
-menuSiteArr.push("http://www.metro.taipei/");
-menuSiteArr.push("https://www.krtco.com.tw/");
-menuSiteArr.push("https://www.tymetro.com.tw");
-menuSiteArr.push("http://www.thsrc.com.tw");
-menuSiteArr.push("http://www.railway.gov.tw/tw/");
 
 ion.sound({
     sounds: [
         {name: "audience_boo"},
-        {name: "clapping_and_cheers"}
+        {name: "clapping_and_cheers"},
+        {name: "clapping"},
     ],
 
     path: "sound/",
